5d3ef77419f50b7155b519d727fed5a71a1733c9
willbush <will.g.bush@gmail.com>
2019-05-19 16:01:28 -0500

    add test cases

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,21 @@
 module Main where
 
 import           RIO
-import           Say (say)
+import           RIO.List           as L
+import           RIO.Text           as T
+import           Say                (say, sayString)
+import           System.Environment (getArgs)
 
 main :: IO ()
-main = say "Hello, Haskell!"
+main = do
+  args <- getArgs
+  let path = fromMaybe "./test-cases/A1.txt" (L.headMaybe args)
+  ls <- toStrippedLines <$> readFileUtf8 path
+  mapM_ say ls
+  sayString path
+
+-- | Takes text and splits it into lines where there are new lines (Windows or
+-- Unix line endings). In addition, each line is stripped (i.e. trimmed) of
+-- white space at the beginning and end.
+toStrippedLines :: Text -> [Text]
+toStrippedLines = (T.strip <$>) . T.linesCR
